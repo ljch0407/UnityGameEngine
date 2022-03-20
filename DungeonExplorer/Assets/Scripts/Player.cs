5e60626c340d186f28298d3de2c48f17433378ed
ljch0407 <ljch0407@gmail.com>
2022-03-20 23:46:45 +0900

    test용도 - 플레이어 움직임 + 회전 + 카메라 움직임

--- DungeonExplorer/Assets/Scripts/Player.cs
+++ DungeonExplorer/Assets/Scripts/Player.cs
@@ -9,19 +9,28 @@ public class Player : MonoBehaviour
     private float hAxis;
     private float vAxis;
     private Vector3 moveVec;
+    private bool walkbtnDown;
+
+    private Animator anim;
     
-    void Start()
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-
+        walkbtnDown = Input.GetButton("Walk");
+        
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-
-        transform.position += moveVec * speed * Time.deltaTime;
+        
+        transform.position += moveVec * speed * (walkbtnDown ? 0.3f : 1f ) * Time.deltaTime;
+        
+        anim.SetBool("isRun",moveVec != Vector3.zero);
+        anim.SetBool("isWalk",walkbtnDown);
+        
+        transform.LookAt(transform.position + moveVec);
     }
 }
