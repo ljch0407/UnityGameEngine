95bba8256e669af5ada79decc6892b51abea381e
ljch0407 <ljch0407@gmail.com>
2022-03-20 23:19:36 +0900

    test용도 - 플레이어 이동 스크립트

---
+++ DungeonExplorer/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int speed;
+    
+    private float hAxis;
+    private float vAxis;
+    private Vector3 moveVec;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+}
