7bbe125cc02a015e8b04e1924d7e85f92859b228
ljch0407 <ljch0407@gmail.com>
2022-03-24 23:48:07 +0900

    플레이어 이동함수 translate로 변경(카메라 방향으로 forward 움직임) + 점프구현 + Wall_Material생성

--- DungeonExplorer/Assets/Scripts/Player.cs
+++ DungeonExplorer/Assets/Scripts/Player.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,28 +11,75 @@ public class Player : MonoBehaviour
     private float hAxis;
     private float vAxis;
     private Vector3 moveVec;
+    private Vector3 moveDir;
     private bool walkbtnDown;
 
+    private bool isJump;
+    private bool jDown;
     private Animator anim;
-    
+    private Rigidbody rigid;
+
+
+    public int jumpPower;
+    public Camera cam;
+
     void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         walkbtnDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis);
         
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-        
-        transform.position += moveVec * speed * (walkbtnDown ? 0.3f : 1f ) * Time.deltaTime;
-        
+        transform.Translate(moveVec*speed*Time.deltaTime,Space.Self);
         anim.SetBool("isRun",moveVec != Vector3.zero);
         anim.SetBool("isWalk",walkbtnDown);
-        
-        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Turn()
+    { 
+        var offset = cam.transform.forward;
+        offset.y = 0;
+        transform.LookAt(transform.position + offset);
+
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * jumpPower,ForceMode.Impulse);
+            anim.SetBool("isJump",true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump",false);
+            isJump = false;
+        }
     }
 }
